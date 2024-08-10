083fe206c89d9fd3a100152252e889fb574b16be
mdsiaofficial <mdsiaofficial@gmail.com>
2024-08-11 00:46:13 +0600

    "Fixed typo in cookie key name, added console logs for token and decoded token, and reformatted comments."

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -6,10 +6,21 @@ const requireAuth = async (req, res, next) => {
     console.log("Middleware running");
   
     // read token off cookies
-    const token = req.cookies.Authoriization;
+    const token = req.cookies.Authorization;
+    console.log(token);
+    // token should look something like this: //
+    // [eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmI3NGUyOWRhMjIyMzU2MTA5YmZmZTUiLCJleHAiOjE3MjU5MDY3MDU4NjEsImlhdCI6MTcyMzMxNDcwNX0.kJ_FQc09p_yLnUu7rHVHllxWBqGwJ_is-3LUlq4C5cU]
+
     // decode token
     const decodedToken = jwt.verify(token, process.env.SECRET);
-  
+    console.log(decodedToken);
+    // decodedToken should look something like this: //
+    // {
+    //   sub: '66b74e29da222356109bffe5', [subject]
+    //   exp: 1725906705861, [expire time]
+    //   iat: 1723314705 [issued at time]
+    // }
+
     // find user using decoded part
     const user = await User.findById(decodedToken.sub);
     if (!user) {
