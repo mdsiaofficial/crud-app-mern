b4d8a84e18db227768a455daaf09f6aa479147eb
mdsiaofficial <mdsiaofficial@gmail.com>
2024-08-11 23:02:14 +0600

    "Added axios.defaults.withCredentials = true;"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
